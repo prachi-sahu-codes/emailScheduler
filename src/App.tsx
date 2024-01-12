7249eab3b5f339b0b6002aab58017474446c6b3e
prachi-sahu-codes <prachisahu998@gmail.com>
2024-01-12 13:14:37 +0530

    feat: routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { Route, Routes } from "react-router";
+import { Home } from "./pages";
 import "./App.css";
 
 function App() {
-  return <>hello</>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
