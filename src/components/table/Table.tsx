6a5ddf39c8713c6e1be8deb4e179375e8fb02937
prachi-sahu-codes <prachisahu998@gmail.com>
2024-01-13 22:00:15 +0530

    feat: table

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -4,7 +4,7 @@ import TableRow from "./TableRow";
 const TableProvider = ({ data }: { data: Email[] }) => {
   return (
     <div className="calcHeight overflow-y-auto">
-      <table id="schedules" className="text-left">
+     {data.length > 0 && <table id="schedules" className="text-left">
       <tr className="h-[42px] bg-[#e1dee9] text-[14px] text-[#333]">
         <th className="px-[24px]">Title</th>
         <th className="px-[24px]">Description</th>
@@ -16,7 +16,7 @@ const TableProvider = ({ data }: { data: Email[] }) => {
       {data.map((schedule: Email) => (
         <TableRow schedule={schedule} key={schedule._id} />
       ))}
-    </table>
+    </table>}
     </div>
   );
 };
