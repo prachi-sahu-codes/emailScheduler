dc8716e353a2a591166513248cedd57e5f72056a
prachi-sahu-codes <prachisahu998@gmail.com>
2024-01-13 21:32:55 +0530

    feat: clear filter functionality

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,17 +1,36 @@
-import { useAppSelector } from "../../app/hooks";
-import { ActionBar, TableProvider } from "../../components";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import { ActionBar, PrimaryBtn, TableProvider } from "../../components";
+import { searchTermUpdate } from "../../features/email/emailSlice";
 
 const Home = () => {
-  const { allEmails } = useAppSelector((state) => state.email);
+  const { allEmails, searchResults, searchValue } = useAppSelector(
+    (state) => state.email
+  );
+  const dispatch = useAppDispatch();
+
+  let arrList = searchValue.length > 0 ? searchResults : allEmails;
+
+  const clickHandler = () => {
+    dispatch(searchTermUpdate(""));
+  };
 
   return (
     <div className="w-screen h-screen pt-[48px] bg-[#F3F3F9] flex">
       <div className="w-[72px] h-full bg-[#391E5A]"></div>
       <div className="w-full">
         <div className="w-full h-[40px] bg-[#D8D2DE]"></div>
-        <div className="py-[20px] px-[24px]">
+        <div className={`py-[20px] px-[24px]`}>
           <ActionBar />
-          <TableProvider data={allEmails} />
+          {arrList.length > 0 ? (
+            <TableProvider data={arrList} />
+          ) : (
+            <div className="flex items-center gap-4">
+              <PrimaryBtn style="px-4" clickHandler={clickHandler}>
+                Clear Filter
+              </PrimaryBtn>
+              <p>No schedules found!</p>
+            </div>
+          )}
         </div>
       </div>
     </div>
