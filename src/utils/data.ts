e12915c5149a8305487d621b2259fbb06824abcd
prachi-sahu-codes <prachisahu998@gmail.com>
2024-01-13 18:53:07 +0530

    chore: time and day data

---
+++ src/utils/data.ts
@@ -0,0 +1,23 @@
+export const timeArr = [
+  { id: 1, time: "08:00AM" },
+  { id: 2, time: "09:00AM" },
+  { id: 3, time: "10:00AM" },
+  { id: 4, time: "11:00AM" },
+  { id: 5, time: "12:00PM" },
+  { id: 6, time: "01:00PM" },
+  { id: 7, time: "02:00PM" },
+  { id: 8, time: "03:00PM" },
+  { id: 9, time: "04:00PM" },
+  { id: 10, time: "05:00PM" },
+  { id: 11, time: "06:00PM" },
+];
+
+export const dayArr = [
+  { id: 1, day: "S", value: "Sun" },
+  { id: 2, day: "M", value: "Mon" },
+  { id: 3, day: "T", value: "Tue" },
+  { id: 4, day: "W", value: "Wed" },
+  { id: 5, day: "T", value: "Thu" },
+  { id: 6, day: "F", value: "Fri" },
+  { id: 7, day: "S", value: "Sat" },
+];
