7fc4f8fee63bb98e3f09a572ee9d7e47f5d30ba9
prachi-sahu-codes <prachisahu998@gmail.com>
2024-01-13 22:00:34 +0530

    style: loader

--- src/components/loader/Loader.tsx
+++ src/components/loader/Loader.tsx
@@ -2,7 +2,7 @@ import { Oval } from "react-loader-spinner";
 
 const Loader = () => {
   return (
-    <div className="fixed bg-gray-700 bg-opacity-30 z-10 top-0 left-0 bottom-0 right-0">
+    <div className="fixed bg-gray-700 bg-opacity-50 z-10 top-0 left-0 bottom-0 right-0">
       <div className="absolute z-50 top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">
         <Oval
           height={80}
