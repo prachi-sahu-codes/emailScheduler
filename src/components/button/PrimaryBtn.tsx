91e8c8fcb0ee57fad5cfbd4623ff8470a9b09f5f
prachi-sahu-codes <prachisahu998@gmail.com>
2024-01-13 18:55:19 +0530

    feat: primary button

--- src/components/button/PrimaryBtn.tsx
+++ src/components/button/PrimaryBtn.tsx
@@ -1,19 +1,24 @@
-import { ReactNode } from "react";
+import { ReactNode, MouseEvent } from "react";
 
 const PrimaryBtn = ({
   children,
   color,
   style,
+  clickHandler,
 }: {
   children: ReactNode;
   color?: boolean;
   style?: string;
+  clickHandler?: (event: MouseEvent<HTMLButtonElement>) => void;
 }) => {
   const bgColor = color
-    ? "bg-[#391E5A] text-[#fff]"
-    : "bg-[#EBE8EF] text-[#333]";
+    ? "bg-[#391E5A] text-[#fff] hover:border-[#000]"
+    : "bg-[#EBE8EF] text-[#333] hover:border-[#dadada]";
   return (
-    <button className={`rounded-[4px] p-[8px] text-[12px] font-semibold ${bgColor} ${style}`}>
+    <button
+      className={`rounded-[4px] p-[8px] text-[12px] font-semibold border ${bgColor} ${style}`}
+      onClick={clickHandler}
+    >
       {children}
     </button>
   );
